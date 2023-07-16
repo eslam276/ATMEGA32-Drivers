13b83a272db313ee299c956983fd90e2f7f5ddfe
eslam276 <128245776+eslam276@users.noreply.github.com>
2023-07-16 09:26:17 +0300

    Initial driver for UART

---
+++ MCAL/5-UART/UART_interface.h
@@ -0,0 +1,16 @@
+/*
+ * UART_interface.h
+ *
+ *       Author: Eslam Nasr
+ */
+
+#ifndef UART_INTERFACE_H_
+#define UART_INTERFACE_H_
+
+
+
+
+void UART_voidInit(void);
+
+
+#endif /* UART_INTERFACE_H_ */
